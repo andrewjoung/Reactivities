1d427745202843a45069c7f03a2be7f1d60b41dc
Andrew Joung <andrew.joung@gmail.com>
2020-04-19 22:34:33 -0700

    add crud operations to client side without data persistence

--- client-app/src/Features/nav/Navbar.tsx
+++ client-app/src/Features/nav/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import {Menu, Container, Button} from "semantic-ui-react";
 
-const Navbar = () => {
+interface IProps {
+    openCreateForm: () => void;
+}
+
+const Navbar: React.FC<IProps> = ({openCreateForm}) => {
     return (
         <Menu fixed='top' inverted>
             <Container>
@@ -11,7 +15,7 @@ const Navbar = () => {
                 </Menu.Item>
                 <Menu.Item name="Activities"/>
                 <Menu.Item>
-                    <Button positive content="Create Activity" />
+                    <Button onClick={() => openCreateForm()}positive content="Create Activity" />
                 </Menu.Item>
             </Container>
         </Menu>
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,21 +1,55 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Icon, List, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import {IActivity} from "../models/activity";
 import Navbar from "../../Features/nav/Navbar";
+import ActivityDashboard from '../../Features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
 
   // [the state, set state]
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(activity => activity.id === id)[0]);
+    setEditMode(false);
+  }
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  }
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)]);
+  }
 
   // useEffect hook => has 3 lifecycle methods
   // Essentially works as component did mount
   // Empty [] ensures that the useEffect runs once
   useEffect(() => {
     axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
-      console.log(response);
-      setActivities(response.data);
+      //console.log(response);
+      let activities: IActivity[] = [];
+      response.data.forEach(activity => {
+        activity.date = activity.date.split('.')[0];
+        activities.push(activity);
+      });
+      setActivities(activities);
     }).catch((err) => {
       console.log(err);
     });  
@@ -23,16 +57,20 @@ const App = () => {
 
   return (
     <div>
-      <Navbar />
+      <Navbar openCreateForm = {handleOpenCreateForm}/>
 
       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+        <ActivityDashboard 
+          activities={activities} 
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity!}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity} 
+        />
       </Container>
     </div>
   );
